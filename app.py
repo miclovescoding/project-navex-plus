96cdb0e47953ca830cb3f993b4132fcf677b5928
miclovescoding <michaelgeng2006@gmail.com>
2026-04-10 21:06:20 +0800

    add comments annotating navex backend code

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request
 from pyproj import Transformer
 
+# creates the web server 
 app = Flask(__name__)
 
+# tool that converts gps latlong to RSO kertau
 transformer = Transformer.from_crs(4326, 3168)
 
+# health check endpoint
 @app.route("/")
 def hello():
     return "Navex+ backend is running"
 
+# turns a python function into a URL endpoint
 @app.route("/convert")
 def convert():
     lat = float(request.args.get("lat"))
